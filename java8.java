6e405449163c32aa3428748f9b85af4ba5ed436c
rahulvadisetty91 <rahulvadis@gmail.com>
2016-12-21 14:01:00 -0500

    Add files via upload

---
+++ java8.java
@@ -0,0 +1,22 @@
+public class Java8Tester {
+   public static void main(String args[]){
+      Java8Tester tester = new Java8Tester();
+		
+      
+      //with parenthesis
+      GreetingService greetService1 = message ->
+      System.out.println("Hello " + message);
+		
+		greetService1.sayMessage("world");
+		
+      () -> System.out.println("Hello World");
+   }
+	
+  
+	
+   interface GreetingService {
+      void sayMessage(String message);
+   }
+	
+  
+}
